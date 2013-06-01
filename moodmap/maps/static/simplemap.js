30f2a038a14e1fdb214a3188ead950d21ecc74e5
Devin Wilson <dmwilson6@gmail.com>
2013-06-01 17:56:33 -0600

    Revised the layout and added containers for the pie chart and tag cloud

--- moodmap/maps/static/simplemap.js
+++ moodmap/maps/static/simplemap.js
@@ -2,8 +2,9 @@ var map;
 
 require(["esri/map", "dojo/domReady!"], function(Map) { 
 	map = new Map("mapDiv", {
-      center: [-56.049, 38.485],
-      zoom: 3,
-      basemap: "streets"
+      center: [-104.87,39.67],
+      zoom: 11,
+      basemap: "streets",
+      autoResize: true
     });
 });
