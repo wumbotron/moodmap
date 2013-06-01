c9fe04cb9d4aa22d87b38346243c3fda229344c6
Andy E <virtualandy@gmail.com>
2013-06-01 11:16:45 -0600

    Add simplemap to views

--- moodmap/maps/views.py
+++ moodmap/maps/views.py
@@ -4,3 +4,8 @@ from django.template.loader import render_to_string
 def index(request):
     """Returns a default view"""
     return HttpResponse(render_to_string('index.html'))
+
+def map(request):
+	"""Returns a map view"""
+	return HttpResponse(render_to_string('simplemap.html'))
+	
