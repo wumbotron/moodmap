433b6ae41841972602af9894a591550240236c4f
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-06-01 19:01:54 -0600

    Fill in data API call

--- moodmap/maps/views.py
+++ moodmap/maps/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
+import models
+import json
+
 def index(request):
     """Returns a default view"""
     return HttpResponse(render_to_string('index.html'))
@@ -18,8 +21,19 @@ def flickr(request):
 	return HttpResponse(render_to_string('flickr.html'))
 	
 def data(request):
-    # Fill in here
-    return HttpResponse()
+    def construct_output(datapoint):
+        """For a given DataPoint, constructs an output dictionary"""
+        return {'user': datapoint.user,
+                'tweet_id': datapoint.tweet_id,
+                'sentiment': datapoint.sentiment,
+                'score': datapoint.score,
+                'datetime': datapoint.datetime.isoformat()
+               }
+
+    NUMPOINTS = 250
+    output = [construct_output(datapoint) for datapoint in
+        models.DataPoint.objects.order_by('tweet_id')[:NUMPOINTS]]
+    return HttpResponse(json.dumps(output))
 
 def tags(request):
     # Fill in here
