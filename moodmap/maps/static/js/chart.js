5d9d0dd28d69c0c75b7af663415eee05f86e6499
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2013-06-06 16:06:00 -0600

    chart update

--- moodmap/maps/static/js/chart.js
+++ moodmap/maps/static/js/chart.js
@@ -3,28 +3,33 @@
 
 });
 
-function refreshChart() {
-	var data = null;
-	if($("#search-query").val() != "") {
-		data = { query: $("#search-query").val() }
-	}
-	$.get("/api/tally.json", data, function(data) {
-      //console.debug('tags', data);
-      var total = data.positive + data.neutral + data.negative;
-      var percents = {"positive": (data.positive / total * 100), "negative": (data.negative / total * 100), "neutral": (data.neutral / total * 100) }
-      //console.log(total);
-      //console.log(data);
-      //console.log(percents);
-      $("#positive").width("" + percents.positive + "%").text(data.positive);
-      $("#neutral").width("" + percents.neutral + "%").text(data.neutral);
-      $("#negative").width("" + percents.negative + "%").text(data.negative);
-      
+function refreshChart() { 
+  var data = null;
+  if($("#search-query").val() != "") {
+    data = { query: $("#search-query").val() }
+  }
+  $.get("/api/tally.json", data, function(data) {
+    //console.debug('tags', data);
+    var total = data.positive + data.neutral + data.negative;
+
+    // Create and populate the data table.
+    var data = google.visualization.arrayToDataTable([
+      ['sentiments', 'values'],
+      ['Positive', data.positive],
+      ['Negative', data.negative],
+      ['Neutral', data.neutral],
+    ]);
+    
+    // Create and draw the visualization.
+    new google.visualization.PieChart(document.getElementById('visualization')).
+        draw(data, {title:"moodmap sentiments"});
+
       $("#stats").html("Total: " + total + "<br>" + "Geo-tagged: " + data.geotagged);
     },
     "json"
   ).fail(function() {
-	console.log("chart load failed");
+  console.log("chart load failed");
   }).done(function() {
-	//console.log("chart done");
+  //console.log("chart done");
   });
 }
