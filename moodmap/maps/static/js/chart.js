226d5630183f5f12c4431bc0a67ea108205a4c6b
William Daniels <Willbdaniels@gmail.com>
2013-06-07 11:34:09 -0600

    Added various U/I updates, as well as fixed the geo-tag bug. yay!

--- moodmap/maps/static/js/chart.js
+++ moodmap/maps/static/js/chart.js
@@ -7,13 +7,14 @@ function refreshChart() {
   var data = null;
   if($("#search-query").val() != "") {
     data = { query: $("#search-query").val() }
+    
   }
   $.get("/api/tally.json", data, function(data) {
     //console.debug('tags', data);
     var total = data.positive + data.neutral + data.negative;
 
     // Create and populate the data table.
-    var data = google.visualization.arrayToDataTable([
+    var Gdata = google.visualization.arrayToDataTable([
       ['sentiments', 'values'],
       ['Positive', data.positive],
       ['Negative', data.negative],
@@ -21,8 +22,10 @@ function refreshChart() {
     ]);
     
     // Create and draw the visualization.
+    console.log("geotag data count:%d" ,data.geotagged);
+    $("#stats").html("Tweet Statistics: " + "Total: " + total + " | " + "Geo-tagged: " + data.geotagged);
     new google.visualization.PieChart(document.getElementById('visualization')).
-      draw(data, {title: 'Sentiments',
+      draw(Gdata, {title: 'Sentiments',
                   titleTextStyle: {color: 'white'},
                   width: '100%', height: 266,
                   is3D:true,
@@ -33,12 +36,14 @@ function refreshChart() {
                   slices: {0: {color: 'green'}, 1: {color:'red'}, 2: {color: 'gray'}}
                  });
 
-      $("#stats").html("Total: " + total + "<br>" + "Geo-tagged: " + data.geotagged);
+      
     },
+
     "json"
   ).fail(function() {
   console.log("chart load failed");
   }).done(function() {
   //console.log("chart done");
   });
+
 }
